3188b90994d879e3dcbd761273eea76fe26cd2cb
javiisebas <javi.sebas@hotmail.es>
2022-04-07 16:34:08 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CalendarApp from './CalendarApp';
+
+import './styles/calendar.css'
+import './styles/modal.css'
+
+
+ReactDOM.render(
+  <CalendarApp />,
+  document.getElementById('root')
+);
+
